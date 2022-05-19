a41485096a8174b13c7685b339c3e7a9a8c23673
Thiago Oliveira <thiago.oliver.siqueira@gmail.com>
2022-05-19 10:39:13 -0300

    feat: 04B - da rota ao conteúdo método index

---
+++ 04-sistema-de-roteamento-02/app.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const seriesRouter = require("./routes/series")
+const app = express()
+const port = 3000 
+
+app.use('/series', seriesRouter)
+app.listen(port, (req,res)=> console.log(`Servidor rodando na porta ${port}`))
---
+++ 04-sistema-de-roteamento-02/controllers/SeriesController.js
@@ -0,0 +1,9 @@
+const series = require('../models/series')
+
+const controller = {
+    index: (req,res)=> res.send(series)
+
+}
+
+
+module.exports = controller
