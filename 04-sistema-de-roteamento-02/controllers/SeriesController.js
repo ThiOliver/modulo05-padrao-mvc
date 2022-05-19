c0687ad73247184c16ce08e62c35c21e789925a4
Thiago Oliveira <thiago.oliver.siqueira@gmail.com>
2022-05-19 10:51:22 -0300

    feat: 04B - adicionando rota com parametro :genero

--- 04-sistema-de-roteamento-02/controllers/SeriesController.js
+++ 04-sistema-de-roteamento-02/controllers/SeriesController.js
@@ -1,7 +1,8 @@
 const series = require('../models/series')
 
 const controller = {
-    index: (req,res)=> res.send(series)
+    index: (req,res)=> res.send(series),
+    porGenero: (req,res)=> res.send(series.filter(serie => serie.genero.toLocaleLowerCase() == req.params.genero.toLocaleLowerCase())),
 
 }
 
--- 04-sistema-de-roteamento-02/models/series.js
+++ 04-sistema-de-roteamento-02/models/series.js
@@ -2,10 +2,12 @@ const series = [
     {
         id: 1,
         nome: "Irmão do Jorel",
+        genero: "Infantil"
     },
     {
         id: 2,
         nome: " Um maluco no pedaço",
+        genero: "Comédia"
     },
 ];
 
